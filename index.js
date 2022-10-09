a4b2a333325e4b22f3b67ece2977fedafc4ff21e
iHaveNoNameSoSorry <48220131+iHaveNoNameSoSorry@users.noreply.github.com>
2022-10-08 23:14:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const hostname = '0.0.0.0';
-const port = 443;
+
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -9,6 +9,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello World');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running at ${port}`);
 });
