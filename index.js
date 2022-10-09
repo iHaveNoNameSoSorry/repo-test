d7f5203f2c9edf4e5b5899cf5ec741470a8e2ab2
iHaveNoNameSoSorry <48220131+iHaveNoNameSoSorry@users.noreply.github.com>
2022-10-09 14:58:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const http = require('http');
+const db = require('./db');
 
+
+(async () =>  
+{
+    let data = await db.select();
+    console.log(data);
+
+})();
+
+console.log("Inserting log...");
+const log = 
+{
+    data: "Starting database application",
+    timestamp: new Date()
+};
+
+
+(async () => 
+{
+    await db.insert(log);
+    console.log("retrieving db...");
+    let data = await db.select();
+    console.log(data);
+
+})();
 
 const port = process.env.PORT || 3000;
-const test = process.env.myBeautifulVariable || "whoops";
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -11,5 +35,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, () => {
-  console.log(`Server running at ${port}. Let's test it: ${test}`);
+  console.log(`Server running at ${port}.`);
 });
